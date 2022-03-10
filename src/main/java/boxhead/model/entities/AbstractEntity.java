de7c5475fc3534275114447f94086cb0c1591a19
Stefano Vanucci <stefano.vanucci@studio.unibo.it>
2022-03-10 12:29:38 +0100

    Added comment and getEntityType

--- src/main/java/boxhead/model/entities/AbstractEntity.java
+++ src/main/java/boxhead/model/entities/AbstractEntity.java
@@ -10,36 +10,66 @@ public abstract class AbstractEntity implements Entity {
 	private double height;
 	private double width;
 	
-	
 	public AbstractEntity(Point2D position,EntityType entityType) {
 		this.position=position;
 		this.entityType=entityType;
 	}
-
+	
+	/** 
+     * Set entity position
+     * @param position entity's position
+     */
 	public void setPosition(Point2D position) {
 		this.position=position;
 	}
-
+	
+	/** 
+     * Get entity position
+     * @return position entity's position
+     */
 	public Point2D getPosition() {
 		return this.position;
 	}
-
+	
+	/** 
+     * Set the height and the width 
+     * @param height height of the box
+     * @param width width of the box
+     */
 	public void setBoundingBox(double height, double width) {
 		this.height=height;
 		this.width=width;
-
 	}
-
+	
+	/** 
+     * Get the bounding box by creating a new BoundingBox object
+     * @return BoundingBox 
+     */
 	public BoundingBox getBoundingBox() {
 		return new BoundingBox(this.position.getX(),this.position.getY(),this.width,this.height);
 	}
-
+	
+	/** 
+     * Get the height
+     * @return height
+     */
 	public double getHeight() {
 		return this.height;
 	}
-
+	
+	/** 
+     * Get the width
+     * @return width
+     */
 	public double getWidth() {
 		return this.width;
 	}
-
+	
+	/** 
+     * Get the entity type
+     * @return EntityType     
+     */
+	public EntityType getEntityType() {
+		return this.entityType;
+	}
 }

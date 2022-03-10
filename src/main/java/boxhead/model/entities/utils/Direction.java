5f067dd225f1fc82f816075ea0f263cdf57faa40
Stefano Vanucci <stefano.vanucci@studio.unibo.it>
2022-03-10 16:00:18 +0100

    Created ActiveEntity and Direction enum

---
+++ src/main/java/boxhead/model/entities/utils/Direction.java
@@ -0,0 +1,20 @@
+package boxhead.model.entities.utils;
+
+public enum Direction {
+	NORTH(),
+	
+	NORTH_EAST(),
+	
+	EAST(),
+	
+	SOUTH_EAST(),
+	
+	SOUTH(),
+
+	SOUTH_WEST(),
+	
+	WEST(),
+	
+	NORTH_WEST();
+	
+}

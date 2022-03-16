15ce66c48d2db550fadac4c3671da53743de12a0
Nicola Milandri <nicola.milandri@studio.unibo.it>
2022-03-16 11:41:48 +0100

    menu button view added

--- src/main/java/boxhead/view/ViewManager.java
+++ src/main/java/boxhead/view/ViewManager.java
@@ -1,5 +1,8 @@
 package boxhead.view;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -12,15 +15,52 @@ public class ViewManager {
 	private final Scene mainScene;
 	private final Stage mainStage;
 
+	private final static int MENU_BUTTONS_START_X = 100;
+	private final static int MENU_bUTTON_START_Y = 150;
+
+	List<BoxheadButton> menuButtons;
+
 	public ViewManager() {
+		menuButtons = new ArrayList<>();
 		mainPane = new AnchorPane();
 		mainScene = new Scene(mainPane, ViewManager.WIDTH, ViewManager.HEIGHT);
 		mainStage = new Stage();
 		mainStage.setScene(mainScene);
+		createButton();
 	}
 
 	public Stage getMainStage() {
 		return mainStage;
+	}
+
+	private void addMenuButton(final BoxheadButton button) {
+		button.setLayoutX(ViewManager.MENU_BUTTONS_START_X);
+		button.setLayoutY(ViewManager.MENU_bUTTON_START_Y + (menuButtons.size() * 100));
+		menuButtons.add(button);
+		mainPane.getChildren().add(button);
+
+	}
+
+	private void createButton() {
+		createStartButton();
+		createScoreButton();
+		createExitButton();
+
+	}
+
+	private void createStartButton() {
+		final BoxheadButton startButton = new BoxheadButton("START!");
+		addMenuButton(startButton);
+	}
+
+	private void createScoreButton() {
+		final BoxheadButton scoreButton = new BoxheadButton("SCORE");
+		addMenuButton(scoreButton);
+	}
+
+	private void createExitButton() {
+		final BoxheadButton exitButton = new BoxheadButton("EXIT");
+		addMenuButton(exitButton);
 	}
 
 }

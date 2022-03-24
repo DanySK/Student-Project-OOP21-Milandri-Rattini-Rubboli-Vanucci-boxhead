848e7f0a21b8fe69ff3e8a3c06ae65033680752a
Emiliano Rattini <emiliano.rattini@studio.unibo.it>
2022-03-24 18:08:59 +0100

    Writed down Level interface and TileType

---
+++ src/main/java/boxhead/view/world/tile/Tile.java
@@ -0,0 +1,62 @@
+package boxhead.view.world.tile;
+
+import javafx.geometry.Point2D;
+import javafx.scene.image.Image;
+
+/**
+ * Interface to model a tile.
+ */
+public interface Tile {
+	/**
+	 * Enum to model the type of tiles.
+	 */
+	public enum TileType {
+		GROUND(),
+		
+		WALL(),
+		
+		AMMO(),
+		
+		ZOMBIE_SPAWN();
+	}
+	/**
+	 * Method to get the image of the tile.
+	 * @return 
+	 */
+	Image getTile();
+	/**
+	 * Method to get the size of the tile.
+	 * @return 
+	 */
+	double getSize();
+	/**
+	 * Method to set the render scale of the tile.
+	 * @param scale
+	 */
+	void setRenderScale(double scale);
+	/**
+	 * Method to get the render scale of the tile.
+	 * @return
+	 */
+	double getRenderScale();
+	/**
+	 * Method to get the relative position.
+	 * @return
+	 */
+	Point2D getRelativePos();
+	/**
+	 * Method to get the absolute position.
+	 * @return
+	 */
+	Point2D getAbsolutePos();
+	/**
+	 * Method to get the x.
+	 * @return
+	 */
+	double getX();
+	/**
+	 * Method to get the y.
+	 * @return
+	 */
+	double getY();
+}

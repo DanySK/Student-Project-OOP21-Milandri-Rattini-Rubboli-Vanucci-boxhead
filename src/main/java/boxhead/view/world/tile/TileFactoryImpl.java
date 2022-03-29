b7087ccbd6a52e390d735621661e3241e08ee9e2
Nicola Milandri <nicola.milandri@studio.unibo.it>
2022-03-29 15:06:29 +0200

    Created TileFactory and TileFactoryImpl

---
+++ src/main/java/boxhead/view/world/tile/TileFactoryImpl.java
@@ -0,0 +1,98 @@
+package boxhead.view.world.tile;
+
+import java.util.Map;
+import java.util.Set;
+
+import javafx.geometry.Point2D;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class TileFactoryImpl implements TileFactory {
+	private double tileSize;
+	private final TileSet tiles;
+
+	/**
+	 * Constructor, URL of the image for TileSet, size of the tile in the image
+	 *
+	 * @param url
+	 * @param s
+	 */
+
+	public TileFactoryImpl(final String url, final double s) {
+		tileSize = s;
+		tiles = new TileSetImpl(url);
+		tiles.Tiles((int) tileSize);
+		;
+	}
+
+	@Override
+	public void setTileSize(final double size) {
+		tileSize = size;
+
+	}
+
+	@Override
+	public Tile createTile(final Integer t, final Point2D pos, final double s) {
+
+		return new Tile() {
+
+			private final ImageView img = new ImageView(tiles.getTile(t));
+			private final SnapshotParameters SP = new SnapshotParameters();
+			private final Point2D p = pos;
+			private final double size = s;
+			private double scale = 1.0;
+
+			@Override
+			public Image getTile() {
+				return img.snapshot(SP, null);
+			}
+
+			@Override
+			public double getSize() {
+				return size * scale;
+			}
+
+			@Override
+			public void setRenderScale(final double scale) {
+				this.scale = scale;
+				img.setFitHeight(s * scale);
+				img.setFitWidth(s * scale);
+
+			}
+
+			@Override
+			public double getRenderScale() {
+				return scale;
+			}
+
+			@Override
+			public Point2D getRelativePos() {
+				return p.multiply(getSize());
+			}
+
+			@Override
+			public Point2D getPos() {
+				return p;
+			}
+
+			@Override
+			public double getX() {
+				return p.getX();
+			}
+
+			@Override
+			public double getY() {
+				return p.getY();
+			}
+
+		};
+	}
+
+	@Override
+	public Set<Tile> createTiles(final Map<Point2D, Integer> tile, final double s) {
+
+		return null;
+	}
+
+}

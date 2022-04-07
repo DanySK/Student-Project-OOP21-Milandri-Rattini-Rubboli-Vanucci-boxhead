f64d6fe76d0362b7d083bcffbf91b1b4568bf5c9
Nicola Milandri <nicola.milandri@studio.unibo.it>
2022-04-07 14:28:52 +0200

    modified LevelController interface into a LevelController class

--- src/main/java/boxhead/controller/level/LevelController.java
+++ src/main/java/boxhead/controller/level/LevelController.java
@@ -1,5 +1,27 @@
 package boxhead.controller.level;
 
-public interface LevelController {
+import boxhead.model.level.Level;
+import boxhead.model.level.LevelGenerator;
+import boxhead.model.level.LevelGeneratorImpl;
+import boxhead.view.level.LevelView;
+
+/**
+ * Controller for Level and and LevelView
+ *
+ * @author USER
+ *
+ */
+public class LevelController {
+
+	private Level l;
+	private LevelView lv;
+
+	private final LevelGenerator lGenerator;
+
+	public LevelController(final double w, final double h, final double ts, final double renderScale) {
+		lGenerator = new LevelGeneratorImpl();
+		l.setScale(renderScale);
+		lv.SetScale(renderScale);
+	}
 
 }

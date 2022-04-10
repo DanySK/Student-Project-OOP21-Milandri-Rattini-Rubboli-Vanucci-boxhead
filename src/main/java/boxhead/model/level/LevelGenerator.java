1476ffd9b14dcee014c285467f6a88c00e4a8e18
Nicola Milandri <nicola.milandri@studio.unibo.it>
2022-04-10 13:33:09 +0200

    General fixes + new methods implemented

--- src/main/java/boxhead/model/level/LevelGenerator.java
+++ src/main/java/boxhead/model/level/LevelGenerator.java
@@ -1,11 +1,16 @@
 package boxhead.model.level;
 
+import org.apache.commons.math3.util.Pair;
+
+import boxhead.view.level.LevelView;
+
 public interface LevelGenerator {
 
 	/**
+	 * @return
 	 *
 	 *
 	 */
-	public void loadLevel();
+	public Pair<Level, LevelView> loadLevel(final double w, final double h, final double ts);
 
 }

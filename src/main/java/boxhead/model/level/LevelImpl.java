a389423a8fc96d29a04fdf8ef0b2b9daf972bea4
Nicola Milandri <nicola.milandri@studio.unibo.it>
2022-04-10 13:23:20 +0200

    General fixes

--- src/main/java/boxhead/model/level/LevelImpl.java
+++ src/main/java/boxhead/model/level/LevelImpl.java
@@ -1,48 +1,50 @@
 package boxhead.model.level;
 
+import java.awt.geom.Point2D;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import boxhead.model.entities.Wall;
-import boxhead.view.world.tile.Tile.TileType;
-import javafx.geometry.Point2D;
 
 /**
  * Implementation of {@link Level}
  */
 public class LevelImpl implements Level {
 
-	private final Map<Point2D, TileType> blocks;
+	private final Map<Point2D, Integer> blocks;
 	private final List<Wall> walls;
 	private final List<Point2D> zombieSpawns;
 	private final double width;
 	private final double height;
 	private final double tileSize;
 	private double scale = 1.0;
-	
+
 	/**
 	 * Constructor of the level.
+	 *
 	 * @param blocks
 	 * @param width
 	 * @param height
 	 * @param tileSize
 	 */
-	public LevelImpl(final Map<Point2D, TileType> blocks, final double width, final double height, final double tileSize) {
+	public LevelImpl(final Map<Point2D, Integer> blocks, final double width, final double height,
+			final double tileSize) {
 		this.blocks = blocks;
-		this.walls = new LinkedList<>();
-		this.zombieSpawns = new LinkedList<>();
+		walls = new LinkedList<>();
+		zombieSpawns = new LinkedList<>();
 		this.width = width;
 		this.height = height;
 		this.tileSize = tileSize;
-		this.loadObjects();
+		loadObjects();
 	}
+
 	/**
 	 * Internal method to load all the objects.
 	 */
 	private void loadObjects() {
-		blocks.forEach((point, type) -> {
-			switch(type) {
+		blocks.forEach((point, id) -> {
+			switch (type) {
 			case WALL:
 				this.walls.add(new Wall(point, tileSize * scale, tileSize * scale));
 				break;
@@ -52,13 +54,13 @@ public class LevelImpl implements Level {
 			}
 		});
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
-	public Map<Point2D, TileType> getBlocks() {
-		return this.blocks;
+	public Map<Point2D, Integer> getBlocks() {
+		return blocks;
 	}
 
 	/**
@@ -66,7 +68,7 @@ public class LevelImpl implements Level {
 	 */
 	@Override
 	public List<Wall> getWalls() {
-		return this.walls;
+		return walls;
 	}
 
 	/**
@@ -74,7 +76,7 @@ public class LevelImpl implements Level {
 	 */
 	@Override
 	public List<Point2D> getZombieSpawnPoints() {
-		return this.zombieSpawns;
+		return zombieSpawns;
 	}
 
 	/**
@@ -82,7 +84,7 @@ public class LevelImpl implements Level {
 	 */
 	@Override
 	public double getWidth() {
-		return this.width;
+		return width;
 	}
 
 	/**
@@ -90,7 +92,7 @@ public class LevelImpl implements Level {
 	 */
 	@Override
 	public double getHeight() {
-		return this.height;
+		return height;
 	}
 
 	/**
@@ -98,14 +100,14 @@ public class LevelImpl implements Level {
 	 */
 	@Override
 	public double getTileSize() {
-		return this.tileSize;
+		return tileSize;
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
-	public void setScale(double scale) {
+	public void setScale(final double scale) {
 		this.scale = scale;
 	}
 

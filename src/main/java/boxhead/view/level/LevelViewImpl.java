012e5dae7bcb999683d56af6fbfee73da7896cdf
Nicola Milandri <nicola.milandri@studio.unibo.it>
2022-04-11 15:43:28 +0200

    LevelViewImpl implementation

---
+++ src/main/java/boxhead/view/level/LevelViewImpl.java
@@ -0,0 +1,106 @@
+package boxhead.view.level;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import boxhead.view.world.tile.Tile;
+import boxhead.view.world.tile.TileFactory;
+import javafx.geometry.Point2D;
+import javafx.scene.image.Image;
+import javafx.util.Pair;
+
+public class LevelViewImpl implements LevelView {
+
+	private final TileFactory tf;
+	private final Set<Tile> tiles;
+	public static Map<Point2D, Integer> blocks = new HashMap<>();
+	public static Set<Pair<Point2D, Image>> levelRendered;
+	public Map<Point2D, Integer> levelTmp = new HashMap<>();
+	private Set<Pair<Point2D, Image>> levelBackground;
+	public static Point2D pos;
+	public final double ts;
+	private final double w;
+	private final double h;
+	private double scale;
+
+	/**
+	 * Constructor of the levelview
+	 *
+	 * @param level - Map<Point2D, Integer> to be transformed in relative tiles
+	 * @param tf    - tileFactory to create tiles
+	 * @param ts    - tile size
+	 * @param w     - width of the level
+	 * @param h     - height of the level
+	 */
+	public LevelViewImpl(final Map<Point2D, Integer> level, final TileFactory tf, final double ts, final double w,
+			final double h) {
+		this.tf = tf;
+		tiles = tf.createTiles(LevelViewImpl.blocks, ts);
+		this.ts = ts;
+		this.w = w;
+		this.h = h;
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	final public Set<Tile> getLevelMap() {
+		return tiles;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public final Set<Pair<Point2D, Image>> renderLevelMap() {
+		Set<Pair<Point2D, Image>> result;
+		if (LevelViewImpl.levelRendered == null) {
+			result = new HashSet<>();
+			tiles.stream().forEach(t -> {
+				result.add(new Pair<>(t.getRelativePos(), t.getTile()));
+			});
+			LevelViewImpl.levelRendered = result;
+		} else {
+			result = LevelViewImpl.levelRendered;
+		}
+		return result;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public final Set<Pair<Point2D, Image>> renderLevelBackground() {
+		final Set<Pair<Point2D, Image>> result;
+		if (levelBackground == null) {
+			result = new HashSet<>();
+			for (int x = 0; x < w; x++) {
+				for (int y = 0; y < h; y++) {
+					LevelViewImpl.pos = new Point2D(x, y);
+					final Tile t = tf.createTile(LevelViewImpl.blocks.get(LevelViewImpl.pos), LevelViewImpl.pos, ts);
+					result.add(new Pair<>(t.getPos(), t.getTile()));
+				}
+			}
+			levelBackground = result;
+		} else {
+			result = levelBackground;
+		}
+		return result;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void setScale(final double scale) {
+		this.scale = scale;
+		tiles.forEach(t -> t.setRenderScale(scale));
+		LevelViewImpl.levelRendered = null;
+		levelBackground = null;
+
+	}
+}
